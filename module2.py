0a3b2bf2fa69095191bc4db1a8cb1c9d66fad79e
Keenan <KMedley3@gmail.com>
2023-05-20 19:28:11 -0400

    first commit

---
+++ module2.py
@@ -0,0 +1,7 @@
+def circumference():
+    r = float(input('What is the radius? '))
+    circle = (float(2 * 3.14159265359 * r))
+    print(circle)
+    return circle
+
+circumference()
---
+++ newmodule.py
@@ -0,0 +1,13 @@
+def house_area():
+    length = input('What is the length? ')
+    width = input('What is the width? ')
+    
+    area = int(length) * int(width)
+    print(area)
+    return area
+
+
+house_area()
+
+# not sure why second functions starts without being called
+
